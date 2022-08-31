164bccc57e8126617a90ce48f4bed74110798f57
jay-forest <109886995+jay-forest@users.noreply.github.com>
2022-08-31 09:06:50 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 13028 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int cards[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			cards[i] = sc.nextInt();
+		}
+		int sum = 0;
+		int max = 0;
+		for (int i = 0; i < n - 2; i++) {
+			for (int j = i + 1; j < n - 1; j++) {
+				for (int k = j + 1; k < n; k++) {
+					sum = cards[i] + cards[j] + cards[k];
+					if (sum <= m) {
+						max = Math.max(max, sum);
+					}
+				}
+			}
+		}
+		System.out.println(max);
+		sc.close();
+	}
+}
