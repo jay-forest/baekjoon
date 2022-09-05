a32616e6e30c38df1a2c1698058a2b55c5277321
jay-forest <109886995+jay-forest@users.noreply.github.com>
2022-09-05 23:57:29 +0900

    [Bronze V] Title: A+B - 8, Time: 188 ms, Memory: 14464 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int tc = 1; tc<=t; tc++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.printf("Case #%d: %d + %d = %d\n", tc, a, b, a+b);
+		}
+
+	}
+
+}
